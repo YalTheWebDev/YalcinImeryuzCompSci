6e75bf4c9f8ec54eca29c4e5f62ab864b8abd241
Yalcin Imeryuz <yimeryuz@gmail.com>
2021-09-03 16:58:01 +0400

    Some animation updates

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const checkpoint = 350;
- 
+const checkpoint = 100;
+const subtitle_checkpoint = 175;
+
+function pageScroll() {
+  window.scrollBy(0, 0.1);
+  scrolldelay = setTimeout(pageScroll, 5);
+}
+
+pageScroll();
+
 window.addEventListener("scroll", () => {
   const currentScroll = window.pageYOffset;
   if (currentScroll <= checkpoint) {
     opacity = 1 - currentScroll / checkpoint;
+    subtitle_opacity = 0 + currentScroll / checkpoint;
+    text_opacity = 0;
+  } else if (subtitle_opacity !== 0) {
+    subtitle_opacity = 1 - currentScroll / subtitle_checkpoint;
+    text_opacity = 0 + currentScroll / subtitle_checkpoint;
   } else {
     opacity = 0;
+    subtitle_opacity = 1;
   }
   document.querySelector(".fade").style.opacity = opacity;
+  document.querySelector(".fade-sub-title").style.opacity = subtitle_opacity;
+  document.querySelector(".down").style.opacity = text_opacity;
 });
