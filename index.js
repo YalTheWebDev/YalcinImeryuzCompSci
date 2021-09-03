0a7a100233c97b65d36501f2586e86e018f16868
Yalcin Imeryuz <yimeryuz@gmail.com>
2021-09-03 15:14:17 +0400

    Added Files.

---
+++ index.js
@@ -0,0 +1,11 @@
+const checkpoint = 350;
+ 
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= checkpoint) {
+    opacity = 1 - currentScroll / checkpoint;
+  } else {
+    opacity = 0;
+  }
+  document.querySelector(".fade").style.opacity = opacity;
+});
